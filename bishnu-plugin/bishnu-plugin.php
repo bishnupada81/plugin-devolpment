e6dba904bc1ad2a19e8473d1eeb4b77c92d60baa
bishnupada81 <53900688+bishnupada81@users.noreply.github.com>
2020-06-22 15:46:37 +0530

    Add files via upload

---
+++ bishnu-plugin/bishnu-plugin.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @package BishnuPlugin
+ */
+/*
+    Plugin Name: Bishnu Plugin
+    Plugin URI:https://github.com/bishnupada81/plugin-devolpment.git
+    Description:This is my first attemp on writting a coustom plugin.
+    Version: 1.0.0
+    Author: Alessandro 'Bishnu' Castellain.
+    Author URI: http://alecadd.com
+    License: GPL v2 on Later.
+    Text Domain: bishnu-plugin
+*/
